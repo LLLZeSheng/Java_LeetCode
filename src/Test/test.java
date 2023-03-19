cb8bab2ddd159dfc21a321a82e18f278c049156b
LLLZeSheng <lllzesheng@163.com>
2023-03-19 13:12:06 +0800

    firstly commit

---
+++ src/Test/test.java
@@ -0,0 +1,15 @@
+package Test;
+
+/**
+ * \* Created with IntelliJ IDEA.
+ * \* User: LZS
+ * \* Date: 2023/3/19
+ * \* Time: 13:09
+ * \* Description:
+ * \
+ */
+public class test {
+    public static void main(String[] args) {
+        System.out.println("Hello Java");
+    }
+}
